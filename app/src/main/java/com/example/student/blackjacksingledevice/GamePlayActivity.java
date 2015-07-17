a0303797ade6f8b9b3b5ab6316c5f5d422a83e23
ayahc1093 <ayahc10.93@gmail.com>
2015-07-16 20:52:18 -0500

    All the Java code is here. Need to make fixes and debug

--- app/src/main/java/com/example/student/blackjacksingledevice/GamePlayActivity.java
+++ app/src/main/java/com/example/student/blackjacksingledevice/GamePlayActivity.java
@@ -1,12 +1,14 @@
 package com.example.student.blackjacksingledevice;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
 import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import java.util.ArrayList;
+import java.util.Scanner;
 
 public class GamePlayActivity extends Activity {
 
@@ -24,6 +26,8 @@ public class GamePlayActivity extends Activity {
     LinearLayout myLinearLayout;
     //GridView grid; //"kishmo kain hu"
     //Gallery playersGallery;
+    private static GamePlayActivity game = new GamePlayActivity();
+    Intent intent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -51,7 +55,7 @@ public class GamePlayActivity extends Activity {
             grid.addView(playersGallery);
         }**/
 
-        GamePlayActivity game = new GamePlayActivity();
+
         game.startGame(allNames);
         //grid.addView(myLinearLayout);
     }
@@ -82,6 +86,7 @@ public class GamePlayActivity extends Activity {
             //System.out.println("It is " + playerNames[i] + "'s turn.");
             playersTurn(i, players, deck, playerNames);
         }
+        dealersTurn(players, deck, playerNames);
     }
 
     //populates the available card spaces to with a value of -1
@@ -125,6 +130,7 @@ public class GamePlayActivity extends Activity {
         }
     }
 
+    //gets the number and the suit of each card using an algorithm
     private static String getCardFaceValueText(int[][] playersHands, int player, int card) {
         int cardNumber;
         int suit;
@@ -148,16 +154,181 @@ public class GamePlayActivity extends Activity {
         return cardFaceValue;
     }
 
-    public static void playersTurn(int i, int[][] players,boolean[] deck,String [] playersName) {
-        printCards(players, i, playersName);
+    public void playersTurn(int i, int[][] players,boolean[] deck, ArrayList<String> playersName) {
+        game.printCards(players, playersName);
         //need to put this if statement in the startGame method
         if(isBlackJack(players[i])){
-            System.out.println(playersName[i] + " has BlackJack!!! Your turn is over; You Won! ");
+            Toast.makeText(GamePlayActivity.this, playersName.get(i) + " has BlackJack!! Your turn is over. You Won!", Toast.LENGTH_LONG).show();
             return;
         }
         promptUser(i, players, deck, playersName);
 
     }
+
+    public static boolean isBlackJack(int[] playerHand){
+        boolean oneTenValueCard=false;
+        boolean oneAceCard=false;
+        for(int i = 0; i < 2; i++) {
+            if (playerHand[i]%13==0)
+                oneAceCard=true;
+            else if(playerHand[i]%13>=9)
+                oneTenValueCard=true;
+        }
+        if(oneAceCard && oneTenValueCard)
+            return true;//blackjack can only happen where there are 2 cards and not more.
+        return false;
+    }
+
+    public void promptUser(int currentPlayer, int[][] players, boolean[] deck, ArrayList<String> playersNames) {
+        boolean isHit;
+        do{
+            int sumOfCards = countCards(players[currentPlayer]);
+            if(sumOfCards >  21){
+                System.out.println(playersNames.get(currentPlayer)  + ", you Busted. Your turn is over; You lost ");
+                break;
+            }
+
+            intent = new Intent(this, HitOrStickDialogBox.class);
+            intent.putExtra("current player", playersNames.get(currentPlayer));
+            intent.putExtra("sum of cards", sumOfCards);
+            new HitOrStickDialogBox().show(getFragmentManager(), "My Fragment");
+
+            Bundle bundle = getIntent().getExtras();
+            isHit = bundle.getBoolean("Player choice");
+
+            if(isHit)
+            {
+                hit(deck,players[currentPlayer]);
+                System.out.println("You were dealt a " + getCardFaceValueText(players, currentPlayer, getPlayersFirstEmptyCardIndex(players[currentPlayer])-1));//need to add here if its a 10,11,12 that its king queen or jack
+            }
+            else
+                System.out.println(playersNames.get(currentPlayer) + ", Your turn is over");
+        } while(isHit);
+        // input.close();
+    }
+
+    public static int countCards(int[] cards) {
+        int sum = 0;
+        boolean aceFound=false;
+        int c = 0;  //changed to 0
+        for (int i = 0; i < cards.length && cards[i] != -1 ; i++ ) {//the loop doesn't need to go through the whole array
+            c = cards[i] % 13; //divide to get card number, but it's 1 off (lower because of 0 index)
+            if (c >= 0 && c < 9)
+                sum += c + 1;
+            if (c >= 9 && c <= 12)
+                sum += 10;
+            if (c == 0)
+                aceFound=true;
+        }
+        if (aceFound)
+            sum+= (isSumLessThanEleven(sum)?10:0);//After summing all cards, add ten if there are any aces with room to be high
+        return sum;
+    }
+
+    public static boolean isSumLessThanEleven(int sum) {
+        if (sum < 11)
+            return true;
+        else
+            return false;
+    }
+
+    public static int hit(boolean[] deck, int[] whosTurn) {
+        int x = (int)(Math.random() * 52);
+        while(deck[x] == true) {
+            x = (int)(Math.random() * 52);
+        }
+        deck[x] = true;
+        whosTurn[getPlayersFirstEmptyCardIndex(whosTurn)] = x;
+        return x;
+    }
+
+    private static int getPlayersFirstEmptyCardIndex(int[] whosTurn) {
+        int i;
+        for(i= 0; i < whosTurn.length; i++) {
+            if (whosTurn[i] == -1) {
+                break;
+            }
+        }
+        return i;
+    }
+
+    public void dealersTurn(int [][] players, boolean [] deck, ArrayList<String> playersNames){
+        int dSum = countCards(players[players.length - 1]);
+        boolean isHit;
+
+        System.out.println("It is the dealer's turn. \nThe dealers cards are:"); //added this to print dealers first 2 cards
+        for (int i = 0; i < 2; i++)
+            System.out.println(getCardFaceValueText(players, players.length -1, i));
+        //check for blackjack here!
+        do{
+            isHit = false;
+            if (dSum < 17){
+                hit(deck, players[players.length - 1]);
+                dSum = countCards(players[players.length - 1]);
+                System.out.println("To see what the dealer has dealt, press enter!");
+                Scanner input = new Scanner(System.in);
+                input.nextLine();
+                System.out.println("The dealer was dealt a "
+                        + getCardFaceValueText(players, players.length - 1, getPlayersFirstEmptyCardIndex(players[players.length - 1])-1) + " your cards now"
+                        + " count up to " + dSum);
+                isHit = true;
+            }else if(isThereOneAceHigh(players[players.length - 1], dSum) && dSum == 17){ // this is what the method is expecting... the isThereOneAce method is also expecting to be passed in something
+                hit(deck, players[players.length - 1]);
+                dSum = countCards(players[players.length - 1]);
+                System.out.println("To see what the dealer has dealt, press enter!");
+                Scanner input = new Scanner(System.in);
+                input.nextLine();
+                System.out.println("The dealer was dealt a "
+                        + getCardFaceValueText(players, players.length - 1, getPlayersFirstEmptyCardIndex(players[players.length - 1])-1) + " your cards now"
+                        + " count up to " + dSum);
+                isHit = true;
+            }else{   //stick
+                System.out.println("\nAll players cards will be displayed:");
+                printCards(players, playersNames);
+                if(dSum > 21){
+                    System.out.println("Dealer busted with a total of " + dSum );
+                    for(int i = 0; i < players.length - 1; i++){
+                        if(countCards(players[i]) <= 21)//checks if the player is not busted (used to be in the isPlayerNotBusted method)
+                            System.out.println(playersNames.get(i) + ": won!");
+                        else
+                            System.out.println(playersNames.get(i) + ": lost.");
+                    }
+                }else
+                    for(int i = 0; i < players.length - 1; i++){
+                        if(countCards(players[i]) <= 21){//checks if the player is not busted
+                            if(countCards(players[i]) > dSum)
+                                System.out.println(playersNames.get(i) + ": won!");
+                            else if(countCards(players[i]) == dSum)
+                                System.out.println(playersNames.get(i) + " and the Dealer -- PUSH, DRAW");
+                            else//player has less than dealerSum
+                                System.out.println(playersNames.get(i) + " lost.");
+                        }else
+                            System.out.println(playersNames.get(i) + " lost.");
+                    }
+            }
+        }while(isHit);
+    }
+
+    public static boolean isThereOneAceHigh(int [] dealer, int dealersSum){
+        int sum = 0;
+        boolean aceFound = false;
+        for (int i = 0; i < dealer.length && dealer[i] != -1 ; i++ ) {//the loop doesn't need to go through the whole array
+            int c = dealer[i] % 13; //divide to get card number, but it's 1 off (lower because of 0 index)
+            if (c >= 0 && c < 9)
+                sum += c + 1;
+            else if (c >= 9 && c <= 12)
+                sum += 10;
+            if (c == 0) {
+                aceFound=true;
+                if(isSumLessThanEleven(sum)) //this method used to be called aceIsHigh(int sum)
+                    return true;
+                else
+                    return false;
+            }
+        }
+        return aceFound;
+    }
+
 }
 
     /*public class ImageAdapter extends BaseAdapter {
