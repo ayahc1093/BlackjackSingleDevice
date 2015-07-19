69f1d3d1c2a95f1adf73f81c4b125a7732690be9
ayahc1093 <ayahc10.93@gmail.com>
2015-07-19 10:29:03 -0400

    working gameplayactivity

--- app/src/main/java/com/example/student/blackjacksingledevice/GamePlayActivity.java
+++ app/src/main/java/com/example/student/blackjacksingledevice/GamePlayActivity.java
@@ -11,6 +11,7 @@ import java.util.Scanner;
 
 public class GamePlayActivity extends Activity {
 
+    //it works!!
 
     ArrayList<String> allNames;  //To store values of EditTexts (names)
     String player = "";
